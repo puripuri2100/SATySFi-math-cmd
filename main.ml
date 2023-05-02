fd332e1bf47eba55249504dbf3abe0a0847f3f8c
puripuri2100 <puripuri2100@gmail.com>
2023-05-03 01:12:29 +0900

    update

--- main.ml
+++ main.ml
@@ -1,102 +1,108 @@
 open Printf
 open Str
+open Sys
 
 
-let libdir = "/usr/local/share/satysfi/"
-let filename = "dist/packages/math.satyh"
-let input_file = libdir ^ filename
-let output_file_saty = "main.saty"
-let output_file_md = "math-cmd.md"
+type cmd_info = {
+  name: string;
+  arg: int;
+}
 
 let cmd_list = ref []
 
+
 (*input_fileから一行ずつ取得してcmd_listに格納*)
-let cat filename =
+let parse filename =
+  let re = Str.regexp {|^[^%]*direct[^\\]*\(\\[-a-zA-Z1-9]+\)[^:]*:[^a-z;]+\([a-z; ]*\)[^a-z;]+math-cmd.*$|} in
+  let parse_cmd_info s =
+    if Str.string_match re s 0 then
+      let cmd_name = Str.global_replace re {|\1|} s in
+      let arg_str = Str.global_replace re {|\2|} s in
+      let arg_lst = Str.split (Str.regexp {|;[^a-z; ]*|}) arg_str in
+      if List.for_all (fun s -> String.equal (String.trim s) "math") arg_lst then
+        if String.equal cmd_name "\\and-also" then
+          (* コマンドの中身がスペースであるため、排除 *)
+          None
+        else
+          Some({
+            name = cmd_name;
+            arg  = List.length arg_lst;
+          })
+      else
+        None
+    else
+      None
+  in
   let fin = open_in filename in
   let rec cat_sub () =
-    cmd_list := (input_line fin) :: !cmd_list;
-    cat_sub ()
+    let s = input_line fin in
+    match parse_cmd_info s with
+    | None -> cat_sub ()
+    | Some(info) -> (
+      cmd_list := info :: !cmd_list;
+      cat_sub ()
+    )
   in
     try cat_sub () with End_of_file -> close_in fin
 
-let () = cat input_file
 
-let () = cmd_list := List.rev !cmd_list
+let rec arg_saty n m =
+  if n == m then
+    ""
+  else
+    let s = 97 + n |> Char.chr |> Char.escaped in
+    (Printf.sprintf "{%s}" s) ^ (arg_saty (n + 1) m)
 
-(* `  direct \cmd : [] math-cmd` の行だけ取り出し*)
-let reg = Str.regexp "[^%]* direct .* \[\] math-cmd"
-let direct_line r s = Str.string_match r s 0
-let cmd_list_2 = List.filter (direct_line reg) !cmd_list
-
-
-(* `direct \cmd : [] math-cmd` から`\cmd`以外の部分を消去*)
-let reg_cmd_1 = Str.regexp ":.*"
-let reg_cmd_2 = Str.regexp "direct"
-let cmd_line_1 s = Str.replace_first reg_cmd_1 "" s
-let cmd_line_2 s = Str.replace_first reg_cmd_2 "" s
-let cmd_list_3 = List.map cmd_line_2 (List.map cmd_line_1 cmd_list_2)
-
-
-let message_list = cmd_list_3
-
-
-let message_saty =
-    List.map (fun s -> "|" ^ s ^ "\n") message_list
-      |> List.fold_left (^) ""
-
-
-let cmd_num = ref 1
-
-let s_n_list s =
-  let () = cmd_num := !cmd_num + 1 in
-  (s, !cmd_num)
-
-let message_list_2 = List.map s_n_list message_list
-
-let md_cell (s, n) =
-  "| `" ^ s ^ "` |![" ^ s ^
-    "](https://github.com/puripuri2100/SATySFi-math-cmd/blob/master/img/math-cmd-" ^ string_of_int n ^ ".png) |\n"
-
-let message_md =
-  List.map md_cell message_list_2
+let saty_str import_str cmd_lst =
+  let cmd_lst_s =
+    cmd_lst
+    |> List.map (fun info -> (
+      Printf.sprintf "| %s%s\n" info.name (arg_saty 0 info.arg)
+    ))
     |> List.fold_left (^) ""
+  in
+  Printf.sprintf "@import: math-cmd
+%s
+document ${
+%s
+|}\n" import_str cmd_lst_s
 
 
-
-(*.satyファイルに書き出す内容*)
-let output_list_saty = [
-    "@import: math-cmd";
-    "@require: math";
-    "document${";
-    message_saty;
-    "|}"
-  ]
-let output_saty =
-  List.map (fun s -> s ^ "\n") output_list_saty
+let md_str dir label cmd_lst =
+  let cmd_lst_s =
+    cmd_lst
+    |> List.mapi (fun n info -> (
+      Printf.sprintf "| `%s` | ![%s](./%s/%s-%d.png) |\n" info.name info.name dir label (n+1)
+    ))
     |> List.fold_left (^) ""
+  in
+  Printf.sprintf "
+|コマンド名|結果|
+|:---------|:--------|
+%s\n" cmd_lst_s
 
 
-(*.mdファイルに書き出す内容*)
-let output_list_md = [
-  "|コマンド名|結果|";
-  "|:---------|:--------|";
-    message_md;
-  ]
-let output_md =
-  List.map (fun s -> s ^ "\n") output_list_md
-    |> List.fold_left (^) ""
+let main () =
+  let argv = Sys.argv in
+  let input_path = Array.get argv 1 in
+  let import_str = Array.get argv 2 in
+  let label = Array.get argv 3 in
+  let output_img_dir = Array.get argv 4 in
+  let output_saty_path = label ^ ".saty" in
+  let output_md_path = label ^ ".md" in
+
+  let () = parse input_path in
+  let cmd_lst = List.rev !cmd_list in
+
+  let output_saty = open_out output_saty_path in
+  fprintf output_saty "%s" (saty_str import_str cmd_lst);
+  close_out output_saty;
+
+  let output_md = open_out output_md_path in
+  fprintf output_md "%s" (md_str output_img_dir label cmd_lst);
+  close_out output_md;
+
+  ()
 
 
-
-(*.satyファイルに書き出す*)
-let () =
-  let oc_saty = open_out output_file_saty in
-  fprintf oc_saty "%s" output_saty;
-  close_out oc_saty
-
-
-(*.mdファイルに書き出す*)
-let () =
-  let oc_md = open_out output_file_md in
-  fprintf oc_md "%s" output_md;
-  close_out oc_md;
+let () = main ()
